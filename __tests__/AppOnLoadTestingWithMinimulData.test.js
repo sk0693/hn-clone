6bbfa405b4023cc8ee9332af43c5459ea8764834
Sourabh khurana <sourabhkhurana0693@gmail.com>
2020-05-09 01:05:51 +0530

    made a unit test case which test on app Load testing with minimul data as props for news

---
+++ __tests__/AppOnLoadTestingWithMinimulData.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import App from '../src/App'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+    it('renders without crashing given the required props', () => {
+        const props = {
+            hits: [
+                {
+                    created_at: "2018-03-14T03:50:30.000Z",
+                    title: "Stephen Hawking has died",
+                    url: "http://www.bbc.com/news/uk-43396008",
+                    author: "Cogito",
+                    points: 6015,
+                    story_text: null,
+                    comment_text: null,
+                    num_comments: 436,
+                    story_id: null,
+                    story_title: null,
+                    story_url: null,
+                    parent_id: null,
+                    created_at_i: 1520999430,
+                    relevancy_score: 8012,
+                    objectID: "16582136",
+                }
+            ]
+        }
+        const wrapper = shallow(<App {...props} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+})
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hits: [],
+      hits: props.hits ? props.hits : [],
       page: 0,
       loading: true,
       error: null,
